ae06d5a39c72f418bc42f5c815e114ad611c03d7
FREMAND <alice.fremand@wanadoo.fr>
2016-09-21 14:54:11 +0200

    Creation of the function 'condition_contact' Template3 for the xml template

--- B2d_XML.py
+++ B2d_XML.py
@@ -1,25 +1,17 @@
 """Create xml file"""
 def xml(A,title, abstract,data_type,North,East,South,West,Depth1,Depth2,T1,T2,Creation_date,subject_Study, project_Phase, location, variables, format1, quality,process, use_lim,access,citation, resource_contact, owner1, owner2, distributor):
-    from os import chdir
     from lxml import etree
     from copy import deepcopy
     #Changement de direction
     import csv
     with open('Contact.csv', 'r') as f:
         reader = csv.reader(f, delimiter=';')
-        contact_list = list(reader)
-    
-    chdir('/Users/Standard/Documents/Programme')
-    
+        contact_list = list(reader) 
     # Les variables sont chargés via le formulaire développé dans test5_widget.py
     # Le chargement 
     #Analyser le fichier xml pour remplacer les attributs voulus
-    doc=etree.parse('Metadata_template2.xml')
+    doc=etree.parse('Metadata_template3.xml')
     root=doc.getroot()
-    tag=[]
-    for child in root:
-        tag.append(child.tag)
-    
     Title_xml=root[8][0][0][0][0][0]
     Abstract_xml=root[8][0][1][0]
     Data_type_xml=root[10][0][0][0][0][0].attrib['codeListValue']
@@ -37,34 +29,34 @@ def xml(A,title, abstract,data_type,North,East,South,West,Depth1,Depth2,T1,T2,Cr
     Use_xml=root[8][0][13][0][0][0]
     Access_xml=root[8][0][13][0][1][0].attrib['codeListValue']
     Citation_xml=root[10][0][1][0][1][0][1][0][0][0]
-    POC_Name_xml=root[3][0][0][0]
-    POC_Organisation_xml=root[3][0][1][0]
-    POC_Adress_xml=root[3][0][3][0][1][0][0][0]
-    POC_City_xml=root[3][0][3][0][1][0][1][0]
-    POC_Postalcode_xml=root[3][0][3][0][1][0][3][0]
-    POC_Country_xml=root[3][0][3][0][1][0][4][0]
-    #POC_email_xml=
-    D_Name_xml=root[8][0][5][0][0][0]
-    D_Organisation_xml=root[8][0][5][0][1][0]
-    D_Phone_xml=root[8][0][5][0][2][0][0][0][0][0]
-    D_Adress_xml=root[8][0][5][0][2][0][1][0][0][0]
-    D_City_xml=root[8][0][5][0][2][0][1][0][1][0]
-    D_Postalcode_xml=root[8][0][5][0][2][0][1][0][2][0]
-    D_Country_xml=root[8][0][5][0][2][0][1][0][3][0]
-    D_email_xml=root[8][0][5][0][2][0][1][0][4][0]
+    POC_Organisation_xml=root[3][0][0][0]
+    POC_Adress_xml=root[3][0][2][0][1][0][0][0]
+    POC_City_xml=root[3][0][2][0][1][0][1][0]
+    POC_Postalcode_xml=root[3][0][2][0][1][0][3][0]
+    POC_Country_xml=root[3][0][2][0][1][0][4][0]
+    POC_email_xml=root[3][0][2][0][1][0][5][0]
+    
+    D_Organisation_xml=root[8][0][5][0][0][0]
+    D_Adress_xml=root[8][0][5][0][1][0][1][0][0][0]
+    D_City_xml=root[8][0][5][0][1][0][1][0][1][0]
+    D_Postalcode_xml=root[8][0][5][0][1][0][1][0][2][0]
+    D_Country_xml=root[8][0][5][0][1][0][1][0][3][0]
+    D_email_xml=root[8][0][5][0][1][0][1][0][4][0]
+
     OW1_Organisation_xml=root[8][0][4][0][0][0]
     OW1_Adress_xml= root[8][0][4][0][1][0][0][0][0][0]
     OW1_City_xml=root[8][0][4][0][1][0][0][0][1][0]
     OW1_Postalcode_xml=root[8][0][4][0][1][0][0][0][2][0]
     OW1_Country_xml=root[8][0][4][0][1][0][0][0][3][0]
     OW1_email_xml= root[8][0][4][0][1][0][0][0][4][0]
+
     OW2_Organisation_xml=root[8][0][4][1][0][0]
     OW2_Adress_xml=root[8][0][4][1][1][0][0][0][0][0]
     OW2_City_xml=root[8][0][4][1][1][0][0][0][1][0]
     OW2_Postalcode_xml=root[8][0][4][1][1][0][0][0][2][0]
     OW2_Country_xml=root[8][0][4][1][1][0][0][0][3][0]
     OW2_email_xml= root[8][0][4][1][1][0][0][0][4][0]
-    
+        
     #Remplisage du fichier xml pour chaque donnée
     
     Title_xml.text=title
@@ -85,16 +77,14 @@ def xml(A,title, abstract,data_type,North,East,South,West,Depth1,Depth2,T1,T2,Cr
     if Len_su>1:
         for i in range(0, Len_su-1):
             root[8][0][9][0][i].addnext(deepcopy(root[8][0][9][0][0]))
-            root[8][0][9][0][i+1][0].text=subject_Study[i+1]
-            
+            root[8][0][9][0][i+1][0].text=subject_Study[i+1]         
     
     Len_pro=len(project_Phase)
     root[8][0][10][0][0][0].text=project_Phase[0]
     if Len_pro>1:
         for i in range(0, Len_pro-1):
             root[8][0][10][0][i].addprevious(deepcopy(root[8][0][10][0][0]))
-            root[8][0][10][0][i+1][0].text=project_Phase[i+1]
-    
+            root[8][0][10][0][i+1][0].text=project_Phase[i+1]   
     
     Len_loc=len(location)
     root[8][0][11][0][0][0].text=location[0]
@@ -127,72 +117,33 @@ def xml(A,title, abstract,data_type,North,East,South,West,Depth1,Depth2,T1,T2,Cr
     Access_xml=access
     Citation_xml.text=citation
     #root[8][0][4].append(deepcopy(root[8][0][4][1]))
-    if resource_contact=='EOST/ IPGS':
-        a=1
-    elif resource_contact=='BRGM':
-        a=2
-    elif resource_contact=='Es Géothermie':
-        a=3
-    elif resource_contact=='GEIE':
-        a=4    
-    if distributor=='EOST/ IPGS':
-        b=1
-    elif distributor=='BRGM':
-        b=2
-    elif distributor=='Es Géothermie':
-        b=3
-    elif distributor=='GEIE':
-        b=4
-    if owner1=='EOST/ IPGS':
-        c=1
-    elif owner1=='BRGM':
-        c=2
-    elif owner1=='Es Géothermie':
-        c=3
-    elif owner1=='GEIE':
-        c=4
-    elif owner1==0:
-        root[8][0][4].remove(root[8][0][4][0])
-        c=0
-    if owner2=='EOST/ IPGS':
-        d=1
-    elif owner2=='BRGM':
-        d=2
-    elif owner2=='Es Géothermie':
-        d=3 
-    elif owner2=='GEIE':
-        d=4
-    elif owner2==0:
+    condition_contact(resource_contact,POC_Organisation_xml,POC_Adress_xml, POC_City_xml,POC_Postalcode_xml , POC_Country_xml, POC_email_xml, contact_list)
+    condition_contact(distributor,D_Organisation_xml,D_Adress_xml, D_City_xml,D_Postalcode_xml , D_Country_xml, D_email_xml, contact_list)
+    condition_contact(owner1,OW1_Organisation_xml,OW1_Adress_xml, OW1_City_xml,OW2_Postalcode_xml , OW1_Country_xml, OW1_email_xml, contact_list)
+    condition_contact(owner2, OW2_Organisation_xml,OW2_Adress_xml, OW2_City_xml,OW2_Postalcode_xml , OW2_Country_xml, OW2_email_xml, contact_list)
+    if owner2==0:
         root[8][0][4].remove(root[8][0][4][1])
-        d=0
-    POC_Organisation_xml.text=contact_list[a][0]
-    POC_Adress_xml.text=contact_list[a][1]
-    POC_City_xml.text=contact_list[a][3]
-    POC_Postalcode_xml.text=contact_list[a][2]
-    POC_Country_xml.text=contact_list[a][4]
-    #POC_email_xml=
-    
-    D_Organisation_xml.text=contact_list[b][0]
-    D_Adress_xml.text=contact_list[b][1]
-    D_City_xml.text=contact_list[b][3]
-    D_Postalcode_xml.text=contact_list[b][2]
-    D_Country_xml.text=contact_list[b][4]
-    D_email_xml.text=contact_list[b][5]
-    
-    if c!=0: 
-        OW1_Organisation_xml.text=contact_list[c][0]
-        OW1_Adress_xml.text=contact_list[c][1]
-        OW1_City_xml.text=contact_list[c][3]
-        OW1_Postalcode_xml.text=contact_list[c][2]
-        OW1_Country_xml.text=contact_list[c][4]
-        OW1_email_xml.text=contact_list[c][5]
-    
-    if d!=0:
-        OW2_Organisation_xml.text=contact_list[d][0]
-        OW2_Adress_xml.text=contact_list[d][1]
-        OW2_City_xml.text=contact_list[d][3]
-        OW2_Postalcode_xml.text=contact_list[d][2]
-        OW2_Country_xml.text=contact_list[d][4]
-        OW2_email_xml.text=contact_list[d][5]
-    
+    if owner1==0:
+        root[8][0][4].remove(root[8][0][4][0])      
     doc.write('Test/Test%d.xml' %A, xml_declaration=True)
+
+def condition_contact(type_contact, organisation, adress, city, post_code, country, mail, contact_list):
+    if type_contact=='EOST/ IPGS':
+        variable=1
+    elif type_contact=='BRGM':
+        variable=2
+    elif type_contact=='Es Géothermie':
+        variable=3 
+    elif type_contact=='GEIE':
+        variable=4
+    else:
+        return
+    organisation.text=contact_list[variable][0]
+    adress.text=contact_list[variable][1]
+    city.text=contact_list[variable][3]
+    post_code.text=contact_list[variable][2]
+    country.text=contact_list[variable][4]
+    mail.text=contact_list[variable][5]
+
+
+    
